ea974b3a212af977860b19008cdb3c0978570ae6
yezixx <skylow917@daum.net>
2024-05-03 15:34:15 +0900

    Create - 투두 추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const mockData = [
   {
@@ -28,10 +28,23 @@ const mockData = [
 function App() {
   const [todos, setTodos] = useState(mockData);
 
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+    setTodos([newTodo, ...todos]);
+    useRef.current++;
+  };
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   );
