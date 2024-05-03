a26bab266dccd369637165f6df42f0d266cc80f1
yezixx <skylow917@daum.net>
2024-05-03 14:45:13 +0900

    기능 구현 준비하기

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,37 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime,
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime,
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime,
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
-      <section>
-        <Header />
-      </section>
-      <section>
-        <Editor />
-      </section>
-      <section>
-        <List />
-      </section>
+      <Header />
+      <Editor />
+      <List />
     </div>
   );
 }
