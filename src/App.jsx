25b603b16bc03391ece13d85de26fbe5195b0971
yezixx <skylow917@daum.net>
2024-05-03 13:46:28 +0900

    UI 구현하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import Header from "./components/Header";
+import Editor from "./components/Editor";
+import List from "./components/List";
 
 function App() {
-  return <>Todo List</>;
+  return (
+    <div className="App">
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Editor />
+      </section>
+      <section>
+        <List />
+      </section>
+    </div>
+  );
 }
 
 export default App;
